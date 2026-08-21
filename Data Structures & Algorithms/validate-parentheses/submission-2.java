806d845af473f7cef0ead27f602e3531b80755dc
Monish Rai <monishrai1801@gmail.com>
2026-08-22 02:20:48 +0530

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+
+        for(int i = 0; i < s.length(); i++){
+            char curr = s.charAt(i);
+            if(curr == '(' || curr == '[' || curr == '{'){
+                st.push(curr);
+            }
+            else{
+                if(st.isEmpty()) return false;
+                if((curr == ')' && st.peek() == '(') || (curr == ']' && st.peek() == '[') || (curr == '}' && st.peek() == '{')){
+                    st.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+
+        return st.size() == 0;
+    }
+}
