b9b83567ab3bf52d4102bd0d498f6c55800843ef
Monish Rai <monishrai1801@gmail.com>
2026-08-21 22:05:46 +0530

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        Set<Integer> ans = new HashSet<>();
+
+        for(int num: nums){
+            if(!ans.add(num)) return true;
+        }
+
+        return false;
+    }
+}
